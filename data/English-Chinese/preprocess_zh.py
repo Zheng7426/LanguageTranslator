eefa011bb302627935cfddf4059ce460cbdd2fdd
Ken Tsoi <kennie98@gmail.com>
2019-11-10 18:03:05 -0500

    Add tokenization for Chinese corpus with jieba word segmentation

---
+++ data/English-Chinese/preprocess_zh.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import jieba
+
+def process_text_file(read_file_path, write_file_path):
+    jieba.load_userdict('jieba/userdict.txt')
+
+    with open('jieba/chinese_stopwords.txt', encoding="utf-8") as f:
+        stop_words = [line.strip() for line in f.readlines()]
+
+    with open(read_file_path, 'r', encoding="utf-8") as r:
+        with open(write_file_path, 'a', encoding="utf-8") as w:
+            for line in r:
+                words = jieba.lcut(line)
+                words = [w for w in words if w not in stop_words]
+                line = ' '.join(words)
+                w.write(line + '\n')
+
+
+if __name__ == '__main__':
+    process_text_file('en-zh\\UNv1.0.en-zh.zh', 'data\\chinese.txt')
+    process_text_file('en-zh\\trainslation2019zh_zh.txt', 'data\\chinese.txt')
---
+++ data/English-Chinese/translation2019zh/DataExtractor.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+data_file_train = "translation2019zh_train.json"
+data_file_valid = "translation2019zh_valid.json"
+out_file_en = "output\\trainslation2019zh_en.txt"
+out_file_zh = "output\\trainslation2019zh_zh.txt"
+
+if not os.path.exists("output"):
+    os.makedirs("output")
+
+with open(out_file_en, 'w', encoding="utf-8") as Of_en, open(out_file_zh, 'w', encoding='utf-8') as Of_zh:
+    with open(data_file_train, 'r', encoding="utf-8") as If:
+        for line in If:
+            Of_en.write(json.loads(line)["english"]+"\n")
+            Of_zh.write(json.loads(line)["chinese"]+"\n")
+    with open(data_file_valid, 'r', encoding="utf-8") as If:
+        for line in If:
+            Of_en.write(json.loads(line)["english"]+"\n")
+            Of_zh.write(json.loads(line)["chinese"]+"\n")
+
